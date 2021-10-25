812c5fe3789126cb3f53cc97571dd27130471409
Jason Yung <json.yung@gmail.com>
2021-10-25 15:20:04 -0400

    use MUI theme to set font

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { Divider } from "@material-ui/core";
+import { Box, colors, createTheme, Divider } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
 
 import "@fontsource/roboto-mono";
 
 import Helloer from "./components/Helloer";
 import store from "./store";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: ['"Roboto Mono"', "Open Sans"].join(",")
+  },
+  palette: {
+    primary: {
+      main: colors.green[500]
+    },
+    secondary: {
+      main: colors.pink[300]
+    }
+  }
+});
+
 const app = (
-  <Provider store={store}>
-    <Helloer emoji="🌎" name="World" />
-    <Divider />
-    <Helloer.Connected />
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Box>
+        <Helloer emoji="🌎" name="World" />
+        <Divider />
+        <Helloer.Connected />
+      </Box>
+    </Provider>
+  </ThemeProvider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
